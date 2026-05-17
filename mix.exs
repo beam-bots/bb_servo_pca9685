af8f136f58a0a1de544fabd7caffdcf09189c7b6
James Harton <james@harton.nz>
2026-05-17 21:31:15 +1200

    chore(deps): bump bb to ~> 0.16 (#34)
    * chore(deps): bump bb to ~> 0.16
    
    * chore(deps): unlock unused ex_cldr_* deps after bb 0.16 bump
    
    * ci: bump elixir-ci.yml to pick up mix-env fix

--- mix.exs
+++ mix.exs
@@ -80,7 +80,7 @@ defmodule BB.Servo.PCA9685.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:bb, bb_dep("~> 0.15.3")},
+      {:bb, bb_dep("~> 0.16")},
       {:pca9685, "~> 1.1"},
 
       # dev/test
